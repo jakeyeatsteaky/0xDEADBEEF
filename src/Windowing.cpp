a936003ec66f0428cc1cd8d6b4dede535408b4ae
JB <jake.baker0630@gmail.com>
2024-05-11 16:28:30 -0700

    finish Window class

--- src/Windowing.cpp
+++ src/Windowing.cpp
@@ -16,5 +16,16 @@ Window::~Window() noexcept
 
 bool Window::Init()
 {
-    
+    SDL_Window* window = SDL_CreateWindow("0xDEADBEEF",
+                                SDL_WINDOWPOS_CENTERED,
+                                SDL_WINDOWPOS_CENTERED,
+                                800,
+                                600,
+                                SDL_WINDOW_SHOWN);
+    if(nullptr == window) {
+        ERR("Failed to create SDL window", SDL_GetError());
+        return false;
+    }
+    // Since the struct SDL_Window is a C struct, (cannot be "constructed"), use .reset();
+    m_window.reset(window);
 }
