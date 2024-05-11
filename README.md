4882f69c053a5c3089e554120928b265c2f48b54
JB <jake.baker0630@gmail.com>
2024-05-11 08:02:08 -0700

    project setup

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # 0xDEADBEEF
 Rendering Engine - VULKAN - C++ - SDL2
+
+
+
+Build using CMAKE --> MSVC or Linux
+
+mkdir build
+cd build
+cmake ..
+make
---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <SDL.h>
+#include <iostream>
+
+
+int main(int /*argc*/, char** /*argv[]*/) {
+
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << "Could not initialize SDL " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow("0xDEADBEEF",
+                                          SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                                          640, 480,
+                                          SDL_WINDOW_SHOWN);
+    if (window == nullptr) {
+        std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Event event;
+    bool running = true;
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
